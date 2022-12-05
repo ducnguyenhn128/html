014fa9927421166d52621dff7b3a0537afb164e0
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2022-12-05 12:03:12 +0700

    aaa

---
+++ B11/bookData.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        "name": "Harry Potter",
+        "completed": true
+    },
+    {
+        "name": "Da Vinci Code",
+        "completed": false
+    },
+    {
+        "name": "Twlight",
+        "completed": false
+    },
+    {
+        "name": "Angels and Demons",
+        "completed": false
+    },
+    {
+        "name": "Lost Symbol,The",
+        "completed": false
+    },
+    {
+        "name": "New Moon",
+        "completed": false
+    }
+];
