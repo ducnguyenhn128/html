8aae2a8ea821527f6ec56f8692900360e1bdc823
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2022-12-09 12:27:11 +0700

    9 12

---
+++ Project/sale.js
@@ -0,0 +1,23 @@
+const sale = [
+    {
+        name: "Áo polo can phối 1.DSTP645",
+        price: "420,000₫",
+        link: "https://bucket.nhanh.vn/store/2071/ps/20221012/52351390471_eae4c80956_o__1_.jpg"
+    },
+
+    {
+        name: "Áo polo họa tiết Pattern ngang ngực 1.DSTP621",
+        price: "420,000₫",
+        link: "https://bucket.nhanh.vn/store/2071/ps/20221012/52351814650_0621a5a122_o.jpg"
+    },
+    {
+        name:"Áo polo in tràn họa tiết Unit 1.DSTP646",
+        price: "400.000 đ",
+        link: "https://bucket.nhanh.vn/store/2071/ps/20221013/52288898508_ed1cdb9c89_o.jpg"
+    },
+    {
+        name:"Áo polo trơn bo kẻ ",
+        price:"380.000 đ",
+        link:"https://bucket.nhanh.vn/store/2071/ps/20221012/52307427720_979d4c2935_o.jpg"
+    }
+]
