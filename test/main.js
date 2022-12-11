71638718535853d1dee091e2c1efb3b83ac8834e
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2022-12-11 18:30:12 +0700

    a

--- test/main.js
+++ test/main.js
@@ -1,9 +1,20 @@
-$('.carousel .carousel-item').each(function () {
-    var minPerSlide = 5;
-    var next = $(this).next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+		items.forEach((el) => {
+			const minPerSlide = 4
+			let next = el.nextElementSibling
+			for (var i=1; i<minPerSlide; i++) {
+				if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
     }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
+})
+
+// Instantiate the Bootstrap carousel
+  
+  // for every slide in carousel, copy the next slide's item in the slide.
+  // Do the same for the next, next item.
