c8f268c2626295ce2b4820724d4408f8d877a83b
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2022-12-11 13:21:09 +0700

    a

--- Project/sale.js
+++ Project/sale.js
@@ -26,3 +26,79 @@ const sale = [
 const sanphammoi = [
     
 ];
+
+
+const storeList = [
+    {
+        name: "TORANO 02 CHÙA BỘC",
+        city: "Hà Nội",
+        address: "02 Chùa Bộc, Q. Đống Đa",
+        phone: "097 640 8388",
+        timeOpen: "8h30-22h"
+    },
+    {
+        name: "TORANO 1165 GIẢI PHÓNG",
+        city: "Hà Nội",
+        address: "1165 Giải Phóng, Q. Hoàng Mai, TP. Hà Nội",
+        phone: "0978 592 388",
+        timeOpen: "8h30-22h"
+    },
+    
+    {
+        name: "TORANO 330 NGUYỄN TRÃI",
+        city: "Hà Nội",
+        address: "330 Nguyễn Trãi, Q. Nam Từ Liêm, Hà Nội",
+        phone: "0976 524 886",
+        timeOpen: "8h30-22h"
+    },
+    
+    {
+        name: "TORANO GIA LÂM",
+        city: "Hà Nội",
+        address: "59 Ngô Xuân Quảng, Gia Lâm, Hà Nội",
+        phone: "0976 522 366",
+        timeOpen: "8h30-22h"
+    },
+    {
+        name: "TORANO 183 QUANG TRUNG GÒ VẤP",
+        city: "TPHCM",
+        address: "183 QUANG TRUNG, P.10, Q. Gò Vấp",
+        phone: "0961608182",
+        timeOpen: "8h30-22h"
+    },
+    {
+        name: "TORANO 425A SƯ VẠN HẠNH NỐI DÀI",
+        city: "TPHCM",
+        address: "425A SƯ VẠN HẠNH NỐI DÀI, P.12, Q.10",
+        phone: "0962601218",
+        timeOpen: "8h30-22h"
+    },
+    {
+        name: "TORANO HẢI PHÒNG",
+        city: "Hải Phòng",
+        address: "212 Lê Lợi, P Ngô Quyền",
+        phone: "0963866759",
+        timeOpen: "8h30-22h"
+    },
+    {
+        name: "TORANO BẮC NINH",
+        city: "Bắc Ninh",
+        address: "399 Ngô Gia Tự, P. Tiền An, TP Bắc Ninh (Cạnh FPT Shop)",
+        phone: "0975 908 081",
+        timeOpen: "8h30-22h"
+    },
+    {
+        name: "TORANO TỪ SƠN",
+        city: "Bắc Ninh",
+        address: "75-77 Minh Khai, Đông Ngàn, Từ Sơn, Bắc Ninh",
+        phone: "0978 383 861",
+        timeOpen: "8h30-22h"
+    },
+    {
+        name: "TORANO NINH BÌNH",
+        city: "Ninh Bình",
+        address: "113 Đinh Tiên Hoàng - TP. Ninh Bình",
+        phone: "0966 300 630",
+        timeOpen: "8h30-22h"
+    },
+]
